d430d92af40a18391ac641eb328201eb2342850d
Trochonovitz <mat.trochonowicz@gmail.com>
2021-08-07 18:49:18 +0200

    Updates

---
+++ precache-manifest.dc57659709146a2c6d4e57abee702037.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2ca38d82e864da0e03bafe5a36fc2a",
+    "url": "/agata-bielen-portfolio-react/index.html"
+  },
+  {
+    "revision": "6c643a8ca92fc53a3c9d",
+    "url": "/agata-bielen-portfolio-react/static/css/3.f01e4b95.chunk.css"
+  },
+  {
+    "revision": "19551de706dc85a397bd",
+    "url": "/agata-bielen-portfolio-react/static/css/4.f01e4b95.chunk.css"
+  },
+  {
+    "revision": "fca81529159a5f7260ce",
+    "url": "/agata-bielen-portfolio-react/static/css/5.a1fda8ff.chunk.css"
+  },
+  {
+    "revision": "87fd019f7d79c03b1423",
+    "url": "/agata-bielen-portfolio-react/static/css/main.090f672b.chunk.css"
+  },
+  {
+    "revision": "720fb71ed0e4e587fea9",
+    "url": "/agata-bielen-portfolio-react/static/js/2.8e25009e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/agata-bielen-portfolio-react/static/js/2.8e25009e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c643a8ca92fc53a3c9d",
+    "url": "/agata-bielen-portfolio-react/static/js/3.9e9902da.chunk.js"
+  },
+  {
+    "revision": "19551de706dc85a397bd",
+    "url": "/agata-bielen-portfolio-react/static/js/4.c09a57f3.chunk.js"
+  },
+  {
+    "revision": "fca81529159a5f7260ce",
+    "url": "/agata-bielen-portfolio-react/static/js/5.e6177fc0.chunk.js"
+  },
+  {
+    "revision": "87fd019f7d79c03b1423",
+    "url": "/agata-bielen-portfolio-react/static/js/main.eaa837dd.chunk.js"
+  },
+  {
+    "revision": "96d047cd5b17451e5b48",
+    "url": "/agata-bielen-portfolio-react/static/js/runtime-main.f5f9c235.js"
+  }
+]);
